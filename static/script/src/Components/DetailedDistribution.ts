7d64d0b1305e23932b4c00f70c9b600efed6cc5c
Devin Lange <devinscottlange@gmail.com>
2020-05-06 10:04:11 -0600

    add basic boxplot to detailed distribution widget

--- static/script/src/Components/DetailedDistribution.ts
+++ static/script/src/Components/DetailedDistribution.ts
@@ -17,7 +17,6 @@ export class DetailedDistribution extends BaseWidget<CurveList> {
         this.setLabel();
     }
 
-    
     private _metricDistributionCollectionLevel : MetricDistributionCollectionLevel;
     public get metricDistributionCollectionLevel() : MetricDistributionCollectionLevel {
         return this._metricDistributionCollectionLevel;
@@ -62,6 +61,11 @@ export class DetailedDistribution extends BaseWidget<CurveList> {
 		return this._mainGroupSelect;
     }
 
+    private _boxplotContainerSelect : SvgSelection;
+    public get boxplotContainerSelect() : SvgSelection {
+        return this._boxplotContainerSelect;
+    }    
+
 	private _brushGroupSelect : SvgSelection;
 	public get brushGroupSelect() : SvgSelection {
 		return this._brushGroupSelect;
@@ -85,13 +89,34 @@ export class DetailedDistribution extends BaseWidget<CurveList> {
 	private _brush : d3.BrushBehavior<any>;
 	public get brush() : d3.BrushBehavior<any> {
 		return this._brush;
-	}
-	
+    }
+    
+    private _median : number;
+    public get median() : number {
+        return this._median;
+    }
+
+    private _quartileRange : [number, number];
+    public get quartileRange() : [number, number] {
+        return this._quartileRange;
+    }
+
+    private _whiskerRange : [number, number];
+    public get whiskerRange() : [number, number] {
+        return this._whiskerRange;
+    }
+    
+    
+    private _scatterplotPadding : number;
+    public get scatterplotPadding() : number {
+        return this._scatterplotPadding;
+    }
+    
 
 	protected setMargin(): void
 	{
 		this._margin = {
-			top: 8,
+			top: 6,
 			right: 8,
 			bottom: 56,
 			left: 8
@@ -104,8 +129,12 @@ export class DetailedDistribution extends BaseWidget<CurveList> {
             .attr("width", this.width)
             .attr("height", this.height);
 
+        this._scatterplotPadding = 8;
         this._mainGroupSelect = this.svgSelect.append("g")
-            .attr('transform', `translate(${this.margin.left}, ${this.margin.top})`);
+            .attr('transform', `translate(${this.margin.left}, ${this.margin.top + this.scatterplotPadding})`);
+
+        this._boxplotContainerSelect = this.svgSelect.append('g')
+        .attr('transform', `translate(${this.margin.left}, ${this.margin.top})`);
 
         this._brushGroupSelect = this.svgSelect.append("g")
 			.attr('transform', `translate(${this.margin.left}, ${this.margin.top})`)
@@ -126,11 +155,6 @@ export class DetailedDistribution extends BaseWidget<CurveList> {
             .classed('labelColor', true)
             .classed('noDisp', true)
 			.text(this.attributeKey);
-    }
-    
-    private showLabel(): void
-    {
-        this.xLabelTextSelect.classed('noDisp', false);
     }
 
     public OnDataChange(): void
@@ -154,42 +178,28 @@ export class DetailedDistribution extends BaseWidget<CurveList> {
         {
             this.randomNoiseList.push(Math.random());
         }
-
+        this.updateBoxplotStats();
         this.updateScales();
         this.draw();
         this.showLabel();
     }
-    
-    private draw(): void
+
+    private updateBoxplotStats(): void
     {
-        this.mainGroupSelect.selectAll('circle')
-            .data<NDim>(this.pointCollection.Array)
-          .join('circle')
-            .attr('cx', d => this.scaleX(d.get(this.attributeKey)))
-            .attr('cy', (d, i) => this.scaleY(this.randomNoiseList[i]))
-            .classed('detailedPoint', true)
-            .classed('noDisp', d => !d.inBrush || isNaN(d.get(this.attributeKey)));
+        let validNumbers: number[] = this.pointCollection.Array
+                            .map(d => d.get(this.attributeKey)) // get actual value
+                            .filter(d => !isNaN(d)) // filter out NaN values.
+                            .sort(); // d3.quantile requires it to be sorted. This could technically be done faster without sorting.
 
-            this.drawAxis();
-
-    }
-
-	private drawAxis(): void
-	{
-		this.xAxisGroupSelect
-			.call(d3.axisBottom(this.scaleX));
-
-	}
-
-    public OnResize(): void
-    {
-        this.updateScales();
-        this.draw();
-    }
-
-    public OnBrushChange(): void
-    {
-        this.draw();
+        this._median = d3.median(validNumbers);
+        let lowQuartile = d3.quantile(validNumbers, 0.25);
+        let highQuartile = d3.quantile(validNumbers, 0.75);
+        this._quartileRange = [lowQuartile, highQuartile];
+        
+        let interQuartileRange = highQuartile - lowQuartile;
+        let lowWhisker  = lowQuartile  - 1.5 * interQuartileRange;
+        let highWhisker = highQuartile + 1.5 * interQuartileRange;
+        this._whiskerRange = [lowWhisker, highWhisker];
     }
 
     private updateScales(): void
@@ -202,10 +212,95 @@ export class DetailedDistribution extends BaseWidget<CurveList> {
 
         this._scaleY = d3.scaleLinear<number, number>()
                         .domain([0, 1]) // bounds of Math.random
-                        .range([this.vizHeight, 0]);
+                        .range([this.vizHeight - 2 * this.scatterplotPadding, 0]);
+    }
 
+    private draw(): void
+    {
+
+        // it's probably faster to filter out all the NaNs once than noDisp them all. There might be as many as 50% NaNs
+        let validPoints = this.pointCollection.Array.filter(d => !isNaN(d.get(this.attributeKey)));
+
+        // draw jittered scatterplot
+        this.mainGroupSelect.selectAll('circle')
+            .data<NDim>(validPoints)
+          .join('circle')
+            .attr('cx', d => this.scaleX(d.get(this.attributeKey)))
+            .attr('cy', (d, i) => this.scaleY(this.randomNoiseList[i]))
+            .classed('detailedPoint', true)
+            .classed('noDisp', d => !d.inBrush);
+
+        this.drawBoxplot();       
+        this.drawAxis();
     }
 
 
+    private showLabel(): void
+    {
+        this.xLabelTextSelect.classed('noDisp', false);
+    }
+
+    private drawBoxplot(): void
+    {
+        // IQR Box
+        this.boxplotContainerSelect.selectAll('rect')
+            .data<[number, number]>([this.quartileRange])
+          .join('rect')
+            .attr('x', d => this.scaleX(d[0]))
+            .attr('y', 0)
+            .attr('width', d => this.scaleX(d[1]) - this.scaleX(d[0]))
+            .attr('height', this.vizHeight)
+            .classed('IQR-Box', true);
+
+        // Median
+        this.boxplotContainerSelect.selectAll('.boxplotMedianLine')
+            .data<number>([this.median])
+          .join('line')
+            .attr('x1', d => this.scaleX(d))
+            .attr('y1', 0)
+            .attr('x2', d => this.scaleX(d))
+            .attr('y2', this.vizHeight)
+            .classed('boxplotMedianLine', true);
+
+        // Horizontal whisker lines
+        const vertMiddle = this.vizHeight / 2;
+        this.boxplotContainerSelect.selectAll('.boxplotWhiskers')
+            .data<[number, number]>([[this.whiskerRange[0], this.quartileRange[0]], [this.whiskerRange[1], this.quartileRange[1]]])
+          .join('line')
+            .attr('x1', d => this.scaleX(d[0]))
+            .attr('y1', vertMiddle)
+            .attr('x2', d => this.scaleX(d[1]))
+            .attr('y2', vertMiddle)
+            .classed('boxplotWhiskers', true);
+
+        // vertical whisker endpoints
+        const relativeSize = 0.66; // height of whisker endpoints compared to box height
+        const padSize = this.vizHeight * (1 - relativeSize) / 2;
+        this.boxplotContainerSelect.selectAll('.boxplotWhiskerEnds')
+            .data<number>(this.whiskerRange)
+          .join('line')
+            .attr('x1', d => this.scaleX(d))
+            .attr('y1', padSize)
+            .attr('x2', d => this.scaleX(d))
+            .attr('y2', this.vizHeight - padSize)
+            .classed('boxplotWhiskerEnds', true);
+        }
+
+	private drawAxis(): void
+	{
+		this.xAxisGroupSelect
+			.call(d3.axisBottom(this.scaleX));
+	}
+
+    public OnResize(): void
+    {
+        this.updateScales();
+        this.draw();
+    }
+
+    public OnBrushChange(): void
+    {
+        this.draw();
+    }
 
 }
