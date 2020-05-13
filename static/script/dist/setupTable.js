823f6e3ebad1686366c6016cdc1700b55dab4b2f
Devin Lange <devinscottlange@gmail.com>
2020-05-13 09:42:38 -0600

    tweak column sizes

--- static/script/dist/setupTable.js
+++ static/script/dist/setupTable.js
@@ -10,8 +10,8 @@ fetch('/data/datasetList.json')
         builder
             .column(LineUpJS.buildStringColumn('vizLinkHtml').label('Viz Link').width(100).html())
             .column(LineUpJS.buildStringColumn('displayName').label('Name').width(260))
-            .column(LineUpJS.buildCategoricalColumn('author', data.authorList).color('green').width(200))
-            .column(LineUpJS.buildStringColumn('folder').label('Folder Path').width(200))
+            .column(LineUpJS.buildCategoricalColumn('author', data.authorList).width(100))
+            .column(LineUpJS.buildStringColumn('folder').label('Folder Path').width(350))
             .column(LineUpJS.buildStringColumn('driveLinkHtml').label('Google Drive Link').width(120).html())
             .column(LineUpJS.buildNumberColumn('fileSize', data.sizeRange).label('size (mb)').width(120))
             .column(LineUpJS.buildDateColumn('modifiedDate').label('Last Modified Date').width(120).format("%b %d, %Y",'%Y-%d-%m'));
