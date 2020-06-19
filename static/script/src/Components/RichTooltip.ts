b2ffa914f605d0e2a562714d2d1edc15dd2027b8
Devin Lange <devinscottlange@gmail.com>
2020-06-19 12:23:49 -0600

    add dynamic placement of labels

--- static/script/src/Components/RichTooltip.ts
+++ static/script/src/Components/RichTooltip.ts
@@ -32,10 +32,66 @@ export class RichTooltip
     public Show(htmlString: string, pageX: number, pageY: number): void
     {
         this.container.innerHTML = htmlString;
-        this.container.style.top = pageY + 'px';
-        this.container.style.left = pageX + 'px';
+
+        // need to display as hidden to get width
+        this.container.style.visibility = 'hidden';
         DevlibTSUtil.show(this.container);
+        let boundRect = this.container.getBoundingClientRect();
+        let containerRect = document.body.getBoundingClientRect();
+
+        // Priority for placement is right, below, left, above
+
+        const offset = 16; // space between label and position
+        const edgeMargin = 10; // whitespace required between label and edge of document.
+        let spaceRight = containerRect.right - pageX;
+        let spaceBelow = containerRect.bottom - pageY;
+        let spaceLeft = containerRect.width - spaceRight;
+        let [top, left]: [number, number] = [0, 0];
+        if (spaceRight > boundRect.width + offset + edgeMargin)
+        {
+            [top, left] = this.positionRight(pageX, pageY, boundRect, offset);
+        }
+        else if (spaceBelow > boundRect.height  + offset + edgeMargin)
+        {
+            [top, left] = this.positionBelow(pageX, pageY, boundRect, offset);
+        }
+        else if (spaceLeft > boundRect.width + offset + edgeMargin)
+        {
+            [top, left] = this.positionLeft(pageX, pageY, boundRect, offset);
+        }
+        else
+        {
+            let [top, left] = this.positionAbove(pageX, pageY, boundRect, offset);
+        }
+        this.container.style.top = top + 'px';
+        this.container.style.left = left + 'px';
+        this.container.style.visibility = 'visible';
         
+    }
+
+    private positionRight(pageX: number, pageY: number, boundRect: DOMRect, offset: number): [number, number]
+    {
+        let top = pageY - boundRect.height / 2.0;
+        let left = pageX + offset;
+        return [top, left];
+    }
+    private positionBelow(pageX: number, pageY: number, boundRect: DOMRect, offset: number): [number, number]
+    {
+        let top = pageY + offset;
+        let left = pageX - boundRect.width / 2.0;
+        return [top, left];
+    }
+    private positionLeft(pageX: number, pageY: number, boundRect: DOMRect, offset: number): [number, number]
+    {
+        let top = pageY - boundRect.height / 2.0;
+        let left = pageX - offset - boundRect.width;
+        return [top, left];
+    }
+    private positionAbove(pageX: number, pageY: number, boundRect: DOMRect, offset: number): [number, number]
+    {
+        let top = pageY - offset - boundRect.height;
+        let left = pageX - boundRect.width / 2.0;
+        return [top, left];
     }
 
     public Hide(): void
