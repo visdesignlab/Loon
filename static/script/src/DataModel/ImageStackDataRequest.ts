94fd2feb1462340277d08048d3cd03cbf953024d
Devin Lange <devinscottlange@gmail.com>
2022-01-19 14:28:59 -0700

    cache change to dict + prettier format

--- static/script/src/DataModel/ImageStackDataRequest.ts
+++ static/script/src/DataModel/ImageStackDataRequest.ts
@@ -1,188 +1,151 @@
 import * as d3 from 'd3';
 import { IDBPDatabase, openDB } from 'idb';
-import { load } from "protobufjs";
+import { load } from 'protobufjs';
+import { App } from '../App';
 
-export interface ImageLabels
-{
-    rowList: Row[]
+export interface ImageLabels {
+    rowList: Row[];
 }
 
-export interface Row
-{
-    row: LabelRun[]
+export interface Row {
+    row: LabelRun[];
 }
 
 export interface LabelRun {
-    start: number,
-    length: number,
-    label: number
+    start: number;
+    length: number;
+    label: number;
 }
 
-
-export class ImageStackDataRequest
-{
-    public constructor(driveId: string)
-    {
+export class ImageStackDataRequest {
+    public constructor(driveId: string) {
         this._driveId = driveId;
-        // this._metaDataLoaded = false;
 
         const metaDataFilename = `/data/${this.driveId}/imageMetaData.json`;
         this._jsonPromise = d3.json(metaDataFilename);
 
-        load("/static/protoDefs/RLE.proto", async (err, root) => {
-            if (err)
-            {
+        load('/static/protoDefs/RLE.proto', async (err, root) => {
+            if (err) {
                 throw err;
             }
-            this._imageLabelsMessage = root.lookupType("imageLabels.ImageLabels");
+            this._imageLabelsMessage = root.lookupType(
+                'imageLabels.ImageLabels'
+            );
         });
 
-        openDB('loon-db').then(async dataStore => 
-        {
+        openDB('loon-db').then(async dataStore => {
             this._dataStore = dataStore;
         });
 
-        this._blobArray= [];
-        this._labelArray= [];
-        this._maxBlobCount = 100;
-        this._nextBlobIndex = 0;
-        this._nextLabelIndex = 0;
-        this._maxLabelCount = 100;
+        this._labelCache = new Map();
+        this._blobCache = new Map();
     }
 
-    public async init(): Promise<void>
-    {
+    public async init(): Promise<void> {
         const metaDataFilename = `/data/${this.driveId}/imageMetaData.json`;
         let data = await this.jsonPromise;
         this.initImageMetaData(data);
     }
 
-    private initImageMetaData(data: any): void
-    {
+    private initImageMetaData(data: any): void {
         this._tileWidth = data.tileWidth;
         this._tileHeight = data.tileHeight;
         this._numberOfColumns = data.numberOfColumns;
         this._tilesPerFile = data.tilesPerFile;
-        // this._metaDataLoaded = true;
-        if (data.scaleFactor)
-        {
+        if (data.scaleFactor) {
             this._scaleFactor = data.scaleFactor;
-        }
-        else
-        {
+        } else {
             this._scaleFactor = 1;
         }
     }
-    
-    private _jsonPromise : Promise<any>;
-    public get jsonPromise() : Promise<any> {
+
+    private _jsonPromise: Promise<any>;
+    public get jsonPromise(): Promise<any> {
         return this._jsonPromise;
     }
-    
-    // private _metaDataLoaded : boolean;
-    // public get metaDataLoaded() : boolean {
-    //     return this._metaDataLoaded;
-    // }
 
-    private _driveId : string;
-    public get driveId() : string {
+    private _driveId: string;
+    public get driveId(): string {
         return this._driveId;
-    }    
+    }
 
-    private _tileWidth : number;
-    public get tileWidth() : number {
+    private _tileWidth: number;
+    public get tileWidth(): number {
         return this._tileWidth;
     }
 
-    private _tileHeight : number;
-    public get tileHeight() : number {
+    private _tileHeight: number;
+    public get tileHeight(): number {
         return this._tileHeight;
     }
-    
-    private _numberOfColumns : number;
-    public get numberOfColumns() : number {
+
+    private _numberOfColumns: number;
+    public get numberOfColumns(): number {
         return this._numberOfColumns;
     }
 
-    private _tilesPerFile : number;
-    public get tilesPerFile() : number {
+    private _tilesPerFile: number;
+    public get tilesPerFile(): number {
         return this._tilesPerFile;
     }
 
-    private _maxBlobCount : number;
-    public get maxBlobCount() : number {
-        return this._maxBlobCount;
-    }
-
-    private _maxLabelCount : number;
-    public get maxLabelCount() : number {
-        return this._maxLabelCount;
-    }
- 
-    private _scaleFactor : number;
-    public get scaleFactor() : number {
+    private _scaleFactor: number;
+    public get scaleFactor(): number {
         return this._scaleFactor;
-    }    
-
-    // blob, key, url
-    private _blobArray : [Blob, string, string][];
-    public get blobArray() : [Blob, string, string][] {
-        return this._blobArray;
     }
 
-    private _nextBlobIndex : number;
-    public get nextBlobIndex() : number {
-        return this._nextBlobIndex;
+    private _blobCache: Map<string, { img: Blob; url: string }>;
+    public get blobCache(): Map<string, { img: Blob; url: string }> {
+        return this._blobCache;
+    }
+    public set blobCache(v: Map<string, { img: Blob; url: string }>) {
+        this._blobCache = v;
     }
 
-    // rows, key
-    private _labelArray : [ImageLabels, string][];
-    public get labelArray() : [ImageLabels, string][] {
-        return this._labelArray;
+    private _labelCache: Map<string, ImageLabels>;
+    public get labelCache(): Map<string, ImageLabels> {
+        return this._labelCache;
     }
 
-    private _nextLabelIndex : number;
-    public get nextLabelIndex() : number {
-        return this._nextLabelIndex;
+    private _dataStore: IDBPDatabase<unknown>;
+    public get dataStore(): IDBPDatabase<unknown> {
+        return this._dataStore;
     }
 
-	private _dataStore : IDBPDatabase<unknown>;
-	public get dataStore() : IDBPDatabase<unknown> {
-		return this._dataStore;
-	}
-
-    private _imageLabelsMessage : any;
-    public get imageLabelsMessage() : any {
+    private _imageLabelsMessage: any;
+    public get imageLabelsMessage(): any {
         return this._imageLabelsMessage;
-    }    
+    }
 
-    public async getImage(location: number, frameIndex: number, callback: (top: number, left: number, blob: Blob, imageUrl: string) => void): Promise<void>
-    {
+    public async getImage(
+        location: number,
+        frameIndex: number,
+        callback: (
+            top: number,
+            left: number,
+            blob: Blob,
+            imageUrl: string
+        ) => void
+    ): Promise<void> {
         let [top, left] = this.getTileTopLeft(frameIndex);
         let bundleIndex = Math.floor(frameIndex / this.tilesPerFile);
         let key = [location, bundleIndex].join('-');
-        
-        let cachedElement = this.blobArray.find(d => d[1] === key);
-        if (cachedElement)
-        {
+        if (this.blobCache.has(key)) {
             this.runWithCachedImage(key, top, left, callback);
             return;
         }
         const imgUrl = `/data/${this.driveId}/img_${location}_${bundleIndex}.jpg`;
-
-        const thisIndex = this.nextBlobIndex;
-        this.blobArray[thisIndex] = [null, key, null];
-        this._nextBlobIndex = (this.nextBlobIndex + 1) % this.maxBlobCount;
-
-        if (this.dataStore)
-        {
+        this.blobCache.set(key, { img: null, url: null });
+        if (this.dataStore) {
             // try and get from data store
-            let store = this.dataStore.transaction('images', 'readonly').objectStore('images');
+            let store = this.dataStore
+                .transaction('images', 'readonly')
+                .objectStore('images');
             let blob = await store.get(imgUrl);
-            if (blob)
-            {
+            if (blob) {
                 let url = window.URL.createObjectURL(blob);
-                this.blobArray[thisIndex] = [blob, key, url];
+
+                this.blobCache.set(key, { img: blob, url: url });
                 callback(top, left, blob, url);
                 return;
             }
@@ -190,126 +153,158 @@ export class ImageStackDataRequest
 
         let xhr = new XMLHttpRequest();
         xhr.responseType = 'blob';
-        xhr.onload = () =>
-        {
+        xhr.onload = () => {
             let blob = xhr.response;
-            if (this.dataStore)
-			{
-				this.dataStore.put<any>('images', blob, imgUrl);
-			}
+            if (this.dataStore) {
+                this.dataStore.put<any>('images', blob, imgUrl);
+            }
             let url = window.URL.createObjectURL(blob);
-            this.blobArray[thisIndex] = [blob, key, url];
+            this.blobCache.set(key, { img: blob, url: url });
+
             callback(top, left, blob, url);
-        }
-        xhr.onerror = (e) =>
-        {
-            console.warn('Error Fetching: ' + imgUrl)
+        };
+        xhr.onerror = e => {
+            console.warn('Error Fetching: ' + imgUrl);
             console.warn(e);
-        }
+        };
         xhr.open('GET', imgUrl);
         xhr.send();
         return;
     }
 
-    private runWithCachedImage(key: string, top: number, left: number, callback: (top: number, left: number, blob: Blob, imageUrl: string) => void): void
-    {
-        let cachedElement = this.blobArray.find(d => d[1] === key);
-        if (cachedElement[0])
-        {
-            callback(top, left, cachedElement[0], cachedElement[2]);
-        }
-        else
-        {
+    private runWithCachedImage(
+        key: string,
+        top: number,
+        left: number,
+        callback: (
+            top: number,
+            left: number,
+            blob: Blob,
+            imageUrl: string
+        ) => void
+    ): void {
+        let cachedElement = this.blobCache.get(key);
+        if (cachedElement.img && cachedElement.url) {
+            callback(top, left, cachedElement.img, cachedElement.url);
+        } else {
             setTimeout(() => {
                 this.runWithCachedImage(key, top, left, callback);
             }, 50);
         }
     }
 
-    public getImagePromise(location: number, frameIndex: number): Promise<[number, number, Blob, string]>
-    {
-        return new Promise((resolve, reject) =>
-        {
+    public getImagePromise(
+        location: number,
+        frameIndex: number
+    ): Promise<[number, number, Blob, string]> {
+        return new Promise((resolve, reject) => {
             try {
-                this.getImage(location, frameIndex, (top: number, left: number, blob: Blob, imageUrl: string) =>
-                {
-                    resolve([top, left, blob, imageUrl]);
-                });
+                this.getImage(
+                    location,
+                    frameIndex,
+                    (
+                        top: number,
+                        left: number,
+                        blob: Blob,
+                        imageUrl: string
+                    ) => {
+                        resolve([top, left, blob, imageUrl]);
+                    }
+                );
             } catch (error) {
                 console.error(error);
                 reject();
             }
-
         });
     }
 
-
-    public getTileTopLeft(frameIndex: number): [number, number]
-	{
-		const left: number = (frameIndex % this.numberOfColumns) * this.tileWidth;
-        let top: number = Math.floor((frameIndex % this.tilesPerFile) / this.numberOfColumns) * this.tileHeight;
-		return [top, left];
+    public getTileTopLeft(frameIndex: number): [number, number] {
+        const left: number =
+            (frameIndex % this.numberOfColumns) * this.tileWidth;
+        let top: number =
+            Math.floor(
+                (frameIndex % this.tilesPerFile) / this.numberOfColumns
+            ) * this.tileHeight;
+        return [top, left];
     }
-    
-    public async getLabel(location: number, frameIndex: number, callback: (rowData: ImageLabels, firstIndex: number) => void): Promise<void>
-    {
-        if (!this.imageLabelsMessage)
-        {
-            setTimeout(() =>
-            {
-                this.getLabel(location, frameIndex, callback)
+
+    public async getLabel(
+        location: number,
+        frameIndex: number,
+        callback: (rowData: ImageLabels, firstIndex: number) => void
+    ): Promise<void> {
+        if (!this.imageLabelsMessage) {
+            setTimeout(() => {
+                this.getLabel(location, frameIndex, callback);
             }, 50);
             return;
         }
 
-        let firstIndex: number = (frameIndex % this.tilesPerFile) * this.tileHeight;
+        let firstIndex: number =
+            (frameIndex % this.tilesPerFile) * this.tileHeight;
         let bundleIndex = Math.floor(frameIndex / this.tilesPerFile);
         let key = [location, bundleIndex].join('-');
-        
-        let cachedElement = this.labelArray.find(d => d[1] === key);
-        if (cachedElement)
-        {
-            // todo - similar runWithCached logic
+
+        if (this.labelCache.has(key)) {
             this.runWithCachedLabel(key, firstIndex, callback);
             return;
         }
-        
-        const thisIndex = this.nextLabelIndex;
-        this._nextLabelIndex = (this.nextLabelIndex + 1) % this.maxBlobCount;
-        this.labelArray[thisIndex] = [null, key];
+        await this.runWithoutCachedLabel(
+            location,
+            key,
+            firstIndex,
+            bundleIndex,
+            callback
+        );
+        return;
+    }
+
+    private async runWithoutCachedLabel(
+        location: number,
+        key: string,
+        firstIndex: number,
+        bundleIndex: number,
+        callback: (rowData: ImageLabels, firstIndex: number) => void
+    ): Promise<void> {
+        console.log('NO CACHE');
+
+        this.labelCache.set(key, null);
 
         const labelUrl = `/data/${this.driveId}/label_${location}_${bundleIndex}.pb`;
 
         let buffer;
-        if (this.dataStore)
-        {
-            let store = this.dataStore.transaction('images', 'readonly').objectStore('images');
+        if (this.dataStore) {
+            let store = this.dataStore
+                .transaction('images', 'readonly')
+                .objectStore('images');
             buffer = await store.get(labelUrl);
         }
-        if (!buffer)
-        {
+        if (!buffer) {
             buffer = await d3.buffer(labelUrl);
             await this.dataStore.put<any>('images', buffer, labelUrl);
         }
 
-        // Decode an Uint8Array (browser) or Buffer (node) to a message
-        let message = this.imageLabelsMessage.decode(new Uint8Array(buffer)) as any;
+        let message = this.imageLabelsMessage.decode(
+            new Uint8Array(buffer)
+        ) as any;
 
-        this.labelArray[thisIndex] = [message, key];
+        this.labelCache.set(key, message);
         callback(message, firstIndex);
 
         return;
     }
 
-    private runWithCachedLabel(key: string, firstIndex: number, callback: (rowData: ImageLabels, firstIndex: number) => void): void
-    {
-        let cachedElement = this.labelArray.find(d => d[1] === key);
-        if (cachedElement[0])
-        {
-            callback(cachedElement[0], firstIndex);
-        }
-        else
-        {
+    private runWithCachedLabel(
+        key: string,
+        firstIndex: number,
+        callback: (rowData: ImageLabels, firstIndex: number) => void
+    ): void {
+        console.log('CACHE');
+
+        let cachedElement = this.labelCache.get(key);
+        if (cachedElement !== null) {
+            callback(cachedElement, firstIndex);
+        } else {
             // loading, try again later
             setTimeout(() => {
                 this.runWithCachedLabel(key, firstIndex, callback);
@@ -317,25 +312,33 @@ export class ImageStackDataRequest
         }
     }
 
-    public getLabelPromise(location: number, frameIndex: number): Promise<[ImageLabels, number]>
-    {
-        return new Promise((resolve, reject) =>
-        {
-            this.getLabel(location, frameIndex, (rowData: ImageLabels, firstIndex: number) =>
-            {
-                resolve([rowData, firstIndex]);
-            })
+    public getLabelPromise(
+        location: number,
+        frameIndex: number
+    ): Promise<[ImageLabels, number]> {
+        return new Promise((resolve, reject) => {
+            this.getLabel(
+                location,
+                frameIndex,
+                (rowData: ImageLabels, firstIndex: number) => {
+                    resolve([rowData, firstIndex]);
+                }
+            );
         });
     }
 
-    public static getLabelValue(rowIdx: number, colIdx: number, rowArray: ImageLabels): number
-    {
+    public static getLabelValue(
+        rowIdx: number,
+        colIdx: number,
+        rowArray: ImageLabels
+    ): number {
         // if this is a bottleneck, this could be improved with quicksearch.
         let row: Row = rowArray.rowList[rowIdx];
-        for (let labelRun of row.row)
-        {
-            if (labelRun.start <= colIdx && colIdx < labelRun.start + labelRun.length)
-            {
+        for (let labelRun of row.row) {
+            if (
+                labelRun.start <= colIdx &&
+                colIdx < labelRun.start + labelRun.length
+            ) {
                 return labelRun.label;
             }
         }
