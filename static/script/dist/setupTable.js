53286b928994d9be007d56f51e24fab1bf5ca95d
Devin Lange <devinscottlange@gmail.com>
2022-01-18 17:11:02 -0700

    add button to overview page to lauch dataset list update

--- static/script/dist/setupTable.js
+++ static/script/dist/setupTable.js
@@ -1,20 +1,50 @@
 fetch('/data/datasetList.json')
     .then(response => response.json())
-    .then(data => 
-    {
+    .then(data => {
         console.log(data);
         const builder = LineUpJS.builder(data.datasetList);
 
-
         // manually define columns
         builder
-            .column(LineUpJS.buildStringColumn('vizLinkHtml').label('Viz Link').width(100).html())
-            .column(LineUpJS.buildStringColumn('displayName').label('Name').width(260))
-            .column(LineUpJS.buildCategoricalColumn('author', data.authorList).width(100))
-            .column(LineUpJS.buildStringColumn('folder').label('Folder Path').width(350))
-            .column(LineUpJS.buildStringColumn('driveLinkHtml').label('Google Drive Link').width(120).html())
-            .column(LineUpJS.buildNumberColumn('fileSize', data.sizeRange).label('size (mb)').width(120))
-            .column(LineUpJS.buildDateColumn('modifiedDate').label('Last Modified Date').width(120).format("%b %d, %Y",'%Y-%m-%d'));
+            .column(
+                LineUpJS.buildStringColumn('vizLinkHtml')
+                    .label('Viz Link')
+                    .width(100)
+                    .html()
+            )
+            .column(
+                LineUpJS.buildStringColumn('displayName')
+                    .label('Name')
+                    .width(260)
+            )
+            .column(
+                LineUpJS.buildCategoricalColumn(
+                    'author',
+                    data.authorList
+                ).width(100)
+            )
+            .column(
+                LineUpJS.buildStringColumn('folder')
+                    .label('Folder Path')
+                    .width(350)
+            )
+            .column(
+                LineUpJS.buildStringColumn('driveLinkHtml')
+                    .label('Google Drive Link')
+                    .width(120)
+                    .html()
+            )
+            .column(
+                LineUpJS.buildNumberColumn('fileSize', data.sizeRange)
+                    .label('size (mb)')
+                    .width(120)
+            )
+            .column(
+                LineUpJS.buildDateColumn('modifiedDate')
+                    .label('Last Modified Date')
+                    .width(120)
+                    .format('%b %d, %Y', '%Y-%m-%d')
+            );
 
         const ranking = LineUpJS.buildRanking()
             .supportTypes()
@@ -24,6 +54,7 @@ fetch('/data/datasetList.json')
 
         builder.ranking(ranking);
 
-        const lineup = builder.buildTaggle(document.body); 
-    }
-)
+        const lineup = builder.buildTaggle(
+            document.getElementById('lineup-container')
+        );
+    });
